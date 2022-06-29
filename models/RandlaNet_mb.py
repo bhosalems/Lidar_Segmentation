ea29a07e9bb1a7c4f00c8e43810e2c78d0b6b2fb
Abhishek Kumar <abhishekninr4@gmail.com>
2022-06-29 00:49:12 -0400

    added locse

--- models/RandlaNet_mb.py
+++ models/RandlaNet_mb.py
@@ -1,38 +1,38 @@
 import torch.nn as nn
+import torch
 import sklearn
-from sklearn.neighbors import NearestNeighbors
-
+try:
+    from torch_points import knn
+except (ModuleNotFoundError, ImportError):
+    from torch_points_kernels import knn
 class LocSE(nn.Module):
-    def __init__(self, k):
+    def __init__(self, k, d_out, device):
+        super(LocSE, self).__init__()
         self.k = k
-        self.knn = NearestNeighbors(n_neighbors=k)
-        self.sharedmlp = Shared_MLP(10, kernel_sz=1, )
-
+        self.sharedmlp = Shared_MLP(in_channels=10, out_channels=d_out, kernel_sz=1)
+        self.device = device
     def forward(self, coords, features):
         """
         returns Local Spatial encoding of point cloud such that corresponding features are aware of their
         relative spatial locations
-
         Args:
             coords: x y z co-ordinates for input data, b_size*N*3
             features: Output from sharedMLP is given as features b_size*N*d
-
         Returns: returns a tensor of size #Todo
-
         """
-        self.knn.fit(coords)
-        knn_dist, knn_points = self.knn.kneighbors(coords,return_distance=True)
+        knn_output = knn(coords.cpu().contiguous(), coords.cpu().contiguous(), self.k)
+        knn_points, knn_dist = knn_output
+        B, N, K = knn_points.size()
+        extended_idx = knn_points.unsqueeze(1).expand(B, 3, N, K)
+        extended_coords = knn_points.transpose(-2, -1).unsqueeze(-1).expand(B, 3, N, K)
+        neighbors = torch.gather(extended_coords, 2, extended_idx)
 
-        r = []
-        for i in range(knn_points.shape[0]):
-            for j in range(knn_points[i].shape[0]):
-                pos_enc = coords[i].tolist() + coords[knn_points[i][j]].tolist() + \
-                          (coords[i] - coords[knn_points[i][j]]).tolist() + [knn_dist[i][j]]
-                r.append(pos_enc)
-
-        r = self.sharedmlp(r)
-        f_encoded = r.append(features)
-        return f_encoded
+        spatial_enc = torch.concat((extended_coords,
+                                   neighbors,
+                                   extended_coords-neighbors,
+                                   knn_dist.unsqueeze(-3)),dim=-3).to_device(self.device)
+        mlp_sp_enc = self.sharedmlp(spatial_enc)
+        return torch.concat(mlp_sp_enc,features.expand(B, -1, N, K),dim=-3)
 
 
 # Shared MLP is implemented with 2D convolutions with kernel size 1*1. In shared MLP weights from all the input to the
@@ -41,6 +41,7 @@ class LocSE(nn.Module):
 # naively convovle) we take the 1*1 kernel size which acts as a node in linear layer.
 class Shared_MLP(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_sz, stride=1, batch_norm=False, activation=None):
+        super(Shared_MLP, self).__init__()
         self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_sz,
                               stride=stride)
         if batch_norm:
@@ -48,7 +49,6 @@ class Shared_MLP(nn.Module):
         else:
             self.bn = None
         self.activation = activation
-
     def forward(self, x):
         """
         Input is the input tensor (could be either intermediate features of the point cloud or the point features at the
@@ -62,3 +62,10 @@ class Shared_MLP(nn.Module):
         if self.activation:
             x = self.activation(x)
         return x
+if __name__ == '__main__':
+    import time
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    d_in = 7
+    cloud = 1000*torch.randn(1, 2**16, d_in).to(device)
+    lse = LocSE(16, 20)
+    lse(cloud[..., :3], cloud[..., 3:7] )
