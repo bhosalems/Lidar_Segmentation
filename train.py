c4ce7105011b21e76d020c107225682ba38c933a
bhosalems <10846405+bhosalems@users.noreply.github.com>
2022-07-08 21:34:52 -0400

    -- Evaluation and model checkpoint

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
-from config.config import PATH_TRAIN
+from ast import arg
+from config.config import PATH_TRAIN, PATH_VALID
 from models.RandlaNet_mb import *
 import os
 import torch
@@ -10,8 +11,6 @@ from torch.utils.tensorboard import SummaryWriter
 from utils.utils import *
 from utils.utils import AverageMeter
 from datetime import datetime
-
-
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -26,8 +25,24 @@ def get_args():
     # https://github.com/scaleapi/pandaset-devkit/issues/132
     parser.add_argument('--num_classes', type=int, help='number of classes in dataset', default=43)
     parser.add_argument('--device', type=str, help='cpu/cuda', default='cuda')
-    parser.add_argument('--epochs', type=int, help='number of train epochs', default=2)
+    parser.add_argument('--epochs', type=int, help='number of train epochs', default=20)
+    parser.add_argument('--save_freq', type =int, help='save frequency for model', default = 5)
     return parser.parse_args()
+
+def eval(model, PATH, criterion, args):
+    model.eval()
+    device = args.device
+    pdset_valid = get_data_loader(PATH, args.b_size, args.MX_SZ, args.n_scenes, False)
+    val_itr_loss = AverageMeter()
+    with torch.no_grad():
+        for idx, data in enumerate(pdset_valid):
+            data = (data[0].to(device), data[1].to(device))
+            valid_pts, valid_labels = data
+            valid_labels = valid_labels.squeeze(-1)
+            val_scores = model(valid_pts)
+            val_loss = criterion(val_scores, valid_labels)
+            val_itr_loss.update(val_loss.item())
+    return val_itr_loss
 
 def randla_train(PATH, args):
 
@@ -61,7 +76,7 @@ def randla_train(PATH, args):
 
                 pt_labels = pt_labels.squeeze(-1)
                 # pt_labels = pt_labels - 1
-                print(pt_labels.max(),pt_labels.min())
+                 #print(pt_labels.max(),pt_labels.min())
 
                 opt.zero_grad()
                 scores = model(pt_cloud)
@@ -74,11 +89,25 @@ def randla_train(PATH, args):
                 logging.info(f'itreration: {idx}/{ln} loss : {loss.item()}')
                 loss.backward()
                 opt.step()
-                
+
             writer.add_scalar(itr_loss.avg, e)
-            logging.info('Epoch completed : {e}/{epochs} loss : {itr_loss.avg}')
+            eval_loss = eval(model, PATH_VALID, criterion, args)
+            writer.add_scalar(eval_loss.avg, e)
+            logging.info(f'Epoch completed : {e}/{epochs}     Train_loss : {itr_loss.avg}    Validation_loss : {eval_loss.avg}')
+            
 
-
+            if e%args.save_freq == 0:
+                torch.save(
+                    {'epoch' : e,
+                    'model_state' : model.state_dict(),
+                    'optimizer_state' : opt.state_dict(),
+                    'loss_at_epoch' : {
+                        'train' : itr_loss.avg,
+                        'valid_loss' : eval_loss.avg
+                    }},
+                    f'{log_dir}/saved_models/model_{e}_{str(datetime.now().strftime("%m%d%y_%H%M%S"))}'
+                )
+           
 if __name__ == "__main__":
     args = get_args()
     PATH = PATH_TRAIN  
